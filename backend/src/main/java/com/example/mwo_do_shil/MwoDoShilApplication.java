c4c8c87748ee8ffff40da6d8cf9e155886253aa6
wjdgur778 <wjdgur778@gmail.com>
2026-01-13 12:31:53 +0900

    backend first commit

---
+++ backend/src/main/java/com/example/mwo_do_shil/MwoDoShilApplication.java
@@ -0,0 +1,13 @@
+package com.example.mwo_do_shil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwoDoShilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwoDoShilApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/mwo_do_shil/MwoDoShilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mwo_do_shil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwoDoShilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
