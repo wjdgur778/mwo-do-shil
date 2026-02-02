468846eac49fd689b3201789dc8b4762ff9543be
wjdgur778 <wjdgur778@gmail.com>
2026-02-02 15:17:47 +0900

    [feat] back&front. baseURL 수정

--- frontend/src/services/recommendService.js
+++ frontend/src/services/recommendService.js
@@ -9,7 +9,7 @@ const recommendService = {
   getRecommendations: async (alcohol, bounds) => {
     try {
       // 이미 api 인스턴스에 baseURL이 설정되어 있으므로 상대 경로만
-      const response = await authApi.get(`/recommend/${alcohol}`, {
+      const response = await authApi.get(`/api/recommend/${alcohol}`, {
         params: {
           minX: bounds.minX,
           minY: bounds.minY,
@@ -29,7 +29,7 @@ const recommendService = {
   getRemainingCount: async () => {
     try {
       // authApi 인터셉터가 Firebase 토큰을 자동으로 헤더에 넣어줍니다.
-      const response = await authApi.get("/recommend/remaining");
+      const response = await authApi.get("/api/recommend/remaining");
       return response.data.data; // 예: 2
     } catch (error) {
       console.error("횟수 조회 실패:", error);
@@ -41,7 +41,7 @@ const recommendService = {
   checkRemainingCount: async () => {
     try {
       // authApi 인터셉터가 Firebase 토큰을 자동으로 헤더에 넣어줍니다.
-      const response = await authApi.get("/recommend/check");
+      const response = await authApi.get("/api/recommend/check");
       console.log(response.data);
       return response.data.remaining; // 예: 2
     } catch (error) {
